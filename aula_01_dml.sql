cb284ca86208a5f5fff39ffda53a8334f5a02922
labsfiap <labsfiap@PA304MICRO10.fiap.com.br>
2025-02-10 22:44:22 -0300

    insert pais

--- aula_01_dml.sql
+++ aula_01_dml.sql
@@ -1,1 +1,9 @@
-insert into 
+insert into PAIS (id_pais,nome) VALUES (1,'Colombia');
+
+insert into PAIS (id_pais,nome) VALUES (2,'Brasil');
+
+insert into PAIS (id_pais,nome) VALUES (3,'USA');
+
+insert into PAIS (id_pais,nome) VALUES (4,'Russia');
+
+insert into PAIS (id_pais,nome) VALUES (5,'Egito')
